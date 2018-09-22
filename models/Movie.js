b846839f570b5d0a6c3961b3038b651143316151
Muhammed Enes Şahin <40808896+freeadmiral@users.noreply.github.com>
2018-09-22 23:20:14 +0300

    validation procedures

--- models/Movie.js
+++ models/Movie.js
@@ -5,13 +5,34 @@ const MovieSchema =new Schema({
     director_id: Schema.Types.ObjectId,
     title:{
         type:String,
-        required:true
+        required:[true,'({PATH} ) alanı zorunludur'],
+        maxlength:[15,'({PATH}) alanı ({VALUE}) ,({MAXLENGTH}) karakterden küçük olmalıdır!'],
+        minlength:[3,'({PATH}) alanı ({VALUE}), ({MINLENGTH}) karakterden büyük olmalıdır']
     },
 
-    category:String,
-    country:String,
-    year:Number,
-    imdb_score:Number,
+    category:{
+        type:String,
+        required:true,
+        maxlength:30,
+        minlength:3,
+    },
+    country:{
+        type:String,
+        required:true,
+        maxlength:25,
+        minlength:3,
+    },
+    year:{
+        type:Number,
+        required:true,
+        max:2040,
+        min:1900,
+    },
+    imdb_score:{
+        type:Number,
+        max:10,
+        min:0,
+    },
     createdAt:{
         type:Date,
         default:Date.now

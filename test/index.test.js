3ec4a5268013ad801eaa8ff80c63ee942cd5e773
Muhammed Enes Şahin <40808896+freeadmiral@users.noreply.github.com>
2018-10-02 00:30:11 +0300

    unit test with "mocha"

---
+++ test/index.test.js
@@ -0,0 +1,20 @@
+const chai=require('chai');
+const chaiHttp=require('chai-http');
+const should=chai.should();                     // sonucu şöyle olmalı gibi sonuc verirken.
+
+const server=require('../app.js');
+
+chai.use(chaiHttp);
+
+describe('Node JS Server Test',()=>{  // description or title your test  
+
+    it('(GET /) İndex sayfası çağırımı başarılı',(done)=>{
+        chai.request(server)
+        .get('/')
+        .end((err,res)=>{
+            res.should.have.status(200);
+            done();
+        });
+        
+    });
+});

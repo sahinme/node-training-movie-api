bde7d812998d8749dbb950c17b74ca6729fd5967
Muhammed Enes Şahin <40808896+freeadmiral@users.noreply.github.com>
2018-10-02 01:59:41 +0300

    unit test (GET a movies by given id)

--- test/movie.test.js
+++ test/movie.test.js
@@ -6,7 +6,7 @@ const server=require('../app.js');
 
 chai.use(chaiHttp);
 
-
+let token,movieId;
 
 describe('/api/movies tests',()=>{  
     before((done)=>{
@@ -49,18 +49,41 @@ describe('(/ POST movies)',()=>{
             .send(movie)
             .set('x-access-token',token)
             .end((err,res)=>{
-                res.should.have.status(200),
-                res.body.should.be.a('object'),
-                res.body.should.have.property('title')
-                res.body.should.have.property('director_id')
-                res.body.should.have.property('category')
-                res.body.should.have.property('country')
-                res.body.should.have.property('year')
-                res.body.should.have.property('imdb_score')
+                res.should.have.status(200);
+                res.body.should.be.a('object');
+                res.body.should.have.property('title');
+                res.body.should.have.property('director_id');
+                res.body.should.have.property('category');
+                res.body.should.have.property('country');
+                res.body.should.have.property('year');
+                res.body.should.have.property('imdb_score');
+                movieId=res.body._id;
                     done();
             });  
     });
 });
 
+describe('/ GET/:director_id movie',()=>{
+    it('it should GET movie by the given id',(done)=>{
+        chai.request(server)
+            .get('/api/movies/'+movieId)
+            .set('x-access-token',token)
+            .end((err,res)=>{
+                res.should.have.status(200),
+                res.body.should.be.a('object');
+                res.body.should.have.property('title');
+                res.body.should.have.property('director_id');
+                res.body.should.have.property('category');
+                res.body.should.have.property('country');
+                res.body.should.have.property('year');
+                res.body.should.have.property('imdb_score');
+                res.body.should.have.property('_id').eql(movieId);
+                    done();
+            });
+        
+    });
+
 });
+
+ });
 
